7c9f65530054ca5187048ceb6c72a0d04982944a
adairconlin <adair.conlin@gmail.com>
2021-11-04 15:25:09 -0400

    add search and submit feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,21 @@
+let userFormEl = document.querySelector("#user-form");
+let nameInputEl = document.querySelector("#username");
+
+let formSubmitHandler = function(event) {
+    event.preventDefault();
+    
+    let username = nameInputEl.value.trim();
+
+    if(username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+
 let getUserRepos = function(user) {
     // format the github api url
     let apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -9,5 +27,3 @@ let getUserRepos = function(user) {
         })
     })
 }
-
-getUserRepos("instagram");
